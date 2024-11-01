f70560a5e6898c0d59b471c4f5fd3f9286e7addf
muntasiranik <anik.muntasir005@gmail.com>
2024-11-02 00:22:00 +0600

    add ant design layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router";
 import "./App.css";
 import Navbar from "./Component/Navbar/Navbar";
 import FooterComponent from "./Component/Footer/Footer";
+import Layouts from "./Component/Layout/Layout";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+    <Layouts/>
+      {/* <Navbar />
+      <Outlet /> */}
       {/* <FooterComponent /> */}
     </>
   );
